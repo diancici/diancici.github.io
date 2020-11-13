8655d38583dbf187917c3bcc4f6953898f71be0d
Dian CHEN <chendian2017@gmail.com>
2020-11-13 09:50:15 +0100

    Update

---
+++ build/precache-manifest.870d1a85a86f63bca015c97ed2131ea2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80536a7e27de1b7e87af9462ee61e99c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "963f631eb9805f2e3664",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6f71ee0f05dbc967bf99",
+    "url": "/static/css/main.5a18080e.chunk.css"
+  },
+  {
+    "revision": "963f631eb9805f2e3664",
+    "url": "/static/js/2.a211e9ee.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.a211e9ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f71ee0f05dbc967bf99",
+    "url": "/static/js/main.b30f95f1.chunk.js"
+  },
+  {
+    "revision": "7fd8f8b23b79231f3b83",
+    "url": "/static/js/runtime-main.933364b2.js"
+  },
+  {
+    "revision": "99e6debc7c5db9747f78d513f24ba870",
+    "url": "/static/media/background.99e6debc.jpg"
+  },
+  {
+    "revision": "7d63871efa0bb0226677becd4185b87b",
+    "url": "/static/media/logo.7d63871e.jpg"
+  },
+  {
+    "revision": "2ff3730394271be277a65676451e0a87",
+    "url": "/static/media/personalWeb.2ff37303.jpg"
+  },
+  {
+    "revision": "72fc23ddc15f16ba11ee758a7fdf0303",
+    "url": "/static/media/profile.72fc23dd.jpg"
+  },
+  {
+    "revision": "1221d32282ed1564b448b65d10928880",
+    "url": "/static/media/smarthome.1221d322.jpg"
+  }
+]);
